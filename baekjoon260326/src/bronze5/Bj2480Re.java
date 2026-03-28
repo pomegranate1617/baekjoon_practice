2fa23be0f73752353eef7bffb4dc8038becb3973
pomegranate <pomegranate1617@gmail.com>
2026-03-28 13:43:07 +0900

    Add files via upload

---
+++ baekjoon260326/src/bronze5/Bj2480Re.java
@@ -0,0 +1,63 @@
+package bronze5;
+
+import java.util.Scanner;
+
+public class Bj2480Re {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int dice1 = sc.nextInt();
+		int dice2 = sc.nextInt();
+		int dice3 = sc.nextInt();
+		int[] diceArr = {dice1,dice2,dice3};
+		boolean[] check = new boolean[3];
+		int[] copyArr = diceArr;
+		int count=0;
+		
+		for(int i=0;i<3;i++) {
+			for(int j=0;j<3;j++) {
+				if(i != j && i<j) {
+					boolean compare;
+					if(copyArr[i] != diceArr[j]) {
+						compare = false;
+					}
+					else {
+						compare = true;
+					}
+					check[count]=compare;
+					count++;
+				}
+			}
+		}
+		
+		int trueCnt = 0;
+		for(int i =0;i<check.length;i++) {
+			if(check[i] ==true) {
+				trueCnt++;
+			}
+			
+		}
+		if(trueCnt>=2) {
+			System.out.println(10000+dice1*1000);
+		}
+		else if(trueCnt==1) {
+			int num=0;
+			for(int i =0;i<3;i++) {
+				if(check[i] == true) {
+					num = diceArr[i];
+					break;
+				}
+			}
+			System.out.println(1000+num*100);
+		}
+		else {
+			int max =0;
+			for(int i=0;i<3;i++) {
+				if(max<diceArr[i]) {
+					max = diceArr[i];
+				}
+			}
+			System.out.println(max*100);
+		}
+		
+	}
+}
---
+++ baekjoon260326/src/bronze5/Bj2480Re2.java
@@ -0,0 +1,41 @@
+//package bronze5;
+//
+//import java.util.Scanner;
+//
+//public class Bj2480Re2 {
+//	public static void main(String[] args) {
+//		//주사위는 3개
+//		//같은 눈 비교
+//		Scanner sc = new Scanner(System.in);
+//		int dice1 = sc.nextInt();
+//		int dice2 = sc.nextInt();
+//		int dice3 = sc.nextInt();
+//		int[] diceArr = {dice1,dice2,dice3};
+//		int[] copy= new int[3];
+//		
+//		for(int i=0;i<diceArr.length;i++) {
+//			copy[i] = diceArr[i];
+//		}
+//		
+//		int correctCnt = 0;
+//		int max=0;
+//		for(int i=0;i<copy.length;i++) {
+//			for(int j=0;j<diceArr.length;j++) {
+//				if(i != j && i<j) {
+//					max = copy[i];
+//					correctCnt++;
+//					if(max<)
+//				}
+//			}
+//		}
+//		
+//		if(correctCnt == 2) {
+//			System.out.println(10000+dice1*1000);
+//		}
+//		else if(correctCnt == 1){
+//			System.out.println();
+//		}
+//		else if
+//		
+//	}
+//}
